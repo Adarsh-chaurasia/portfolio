18e2b6cc815d4d4f22e014640f3e0c93272d4ae0
manikantmaurya <64607600+manikantmaurya@users.noreply.github.com>
2020-07-11 20:08:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,9 +56,3 @@ var TxtRotate = function(el, toRotate, period) {
   };
 
 
-let progress = document.getElementById('progressbar');
-        let totalHeight = document.body.scrollHeight - window.innerHeight;
-          window.onscroll = function(){
-        let progressHeight = (window.pageYOffset / totalHeight) * 100;
-        progress.style.height = progressHeight + "%";
-        }
